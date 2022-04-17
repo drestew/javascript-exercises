cfa985271640875ea96b6924a31f5ff9107fd7d8
drestew <drestew48@gmail.com>
2022-04-16 22:32:03 -0400

    Complete exercises 11-13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const peopleAges = people.map((person) => {
+    const ageAtDeath =
+      (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth
+    return { name: person.name, age: ageAtDeath }
+  })
+  const oldestPerson = peopleAges.reduce((acc, cur) =>
+    acc.age > cur.age ? acc : (acc = cur)
+  )
+  return oldestPerson
+}
 
-};
+const people = [
+  {
+    name: 'Carly',
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: 'Ray',
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: 'Jane',
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+findTheOldest(people)
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,37 @@
-const caesar = function() {
-
-};
-
+const caesar = function (string, shiftNum) {
+  const charCodeIntegers = Array.from(Array(26)).map((e, i) => i + 65)
+  const alphabet = charCodeIntegers.map((x) =>
+    String.fromCharCode(x).toLowerCase()
+  )
+  const strArr = string.split('')
+  const newStr = strArr.reduce((acc, cur) => {
+    let newCharIndex = alphabet.indexOf(cur.toLowerCase())
+    if (newCharIndex === -1) {
+      return (acc += cur)
+    } else {
+      if (newCharIndex + shiftNum >= 26) {
+        newCharIndex -= 26
+        if (newCharIndex + shiftNum >= 26) {
+          newCharIndex -= 26
+          if (newCharIndex + shiftNum >= 26) {
+            newCharIndex -= 26
+          }
+        }
+      } else if (newCharIndex + shiftNum < 0) {
+        newCharIndex += 26
+        if (newCharIndex + shiftNum < 0) {
+          newCharIndex += 26
+          if (newCharIndex + shiftNum < 0) {
+            newCharIndex += 26
+          }
+        }
+      }
+      let newStr = alphabet[newCharIndex + shiftNum]
+      return (acc += cur.toUpperCase() === cur ? newStr.toUpperCase() : newStr)
+    }
+  }, '')
+  return newStr
+}
+caesar('Hello, World!', 75)
 // Do not edit below this line
-module.exports = caesar;
+module.exports = caesar
